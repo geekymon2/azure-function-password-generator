2b27f1e0d6ddaefc81a26e8afa0bc14f441bd7ba
geekymon2 <geekymon2@gmail.com>
2024-08-30 16:51:12 +1000

    update

---
+++ PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,13 @@
+namespace GeekyMon2.Azure.Function.PasswordGenerator
+{
+    public class PasswordGenerator
+    {
+        public string GeneratePassword(int length)
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
--- RandomPasswordGenerator.cs
+++ RandomPasswordGenerator.cs
@@ -2,23 +2,39 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using GeekyMon2.Azure.Function.PasswordGenerator;
 
 namespace GeekyMon2.Azure.Function
 {
     public class RandomPasswordGenerator
     {
         private readonly ILogger<RandomPasswordGenerator> _logger;
+        private PasswordGenerator _generator;
+        
 
-        public RandomPasswordGenerator(ILogger<RandomPasswordGenerator> logger)
+        public RandomPasswordGenerator(ILogger<RandomPasswordGenerator> logger, PasswordGenerator generator)
         {
             _logger = logger;
+            _generator = generator;
         }
 
         [Function("RandomPasswordGenerator")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            _logger.LogInformation("C# HTTP trigger function RandomPasswordGenerator request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            dynamic? data = JsonSerializer.Deserialize<JsonNode>(requestBody);
+            string? length = (string?)data?["length"];
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+
+            string password = generator.GeneratePassword(length);
+
+            return response.WriteAsync($"password: {password}");
         }
     }
 }
